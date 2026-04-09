63b9a7117f3c1cff97e68a6a548d10cc83f1c3d3
feliipecardosoo <fglibino@gmail.com>
2026-04-08 22:26:17 -0300

    Finalizando lógica nova - End point API + Email

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,50 +3,28 @@ package main
 import (
 	"context"
 	"log"
-	"time"
-
-	"pg-query-to-email/internal/database"
-	"pg-query-to-email/internal/repository"
+	"pg-query-to-email/internal/client"
+	configenv "pg-query-to-email/internal/config"
+	"pg-query-to-email/internal/service"
 )
 
 func main() {
-	// ──────────────────────────────────────────────────
-	// 1. Database connection
-	// ──────────────────────────────────────────────────
-	dbPoolWrapper := database.NewDatabase()
-	if err := dbPoolWrapper.Connect(); err != nil {
-		log.Fatal(err)
-	}
-	defer dbPoolWrapper.Close()
+	carregaEnv := configenv.NewEnv()
+	carregaEnv.LoadEnv()
 
-	pool := dbPoolWrapper.GetPool()
-
-	// ──────────────────────────────────────────────────
-	// 2. Repository initialization
-	// ──────────────────────────────────────────────────
-	repo := repository.NewQueryRepository(pool)
-
-	// ──────────────────────────────────────────────────
-	// 3. Context
-	// ──────────────────────────────────────────────────
+	//Context
 	ctx := context.Background()
 
-	// ──────────────────────────────────────────────────
-	// 4. Executar query
-	// ──────────────────────────────────────────────────
-	now := time.Now()
-	start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
-	end := start.Add(24 * time.Hour)
+	// Client (HTTP)
+	notifierClient := client.NewNotifierClient()
 
-	users, err := repo.GetUsers(ctx, start, end)
-	if err != nil {
-		log.Fatal(err)
+	// Service
+	notifierService := service.NewNotifierService(notifierClient)
+
+	//  Executar fluxo
+	if err := notifierService.Execute(ctx); err != nil {
+		log.Fatal("erro ao executar notifier:", err)
 	}
 
-	// ──────────────────────────────────────────────────
-	// 5. Ver resultado
-	// ──────────────────────────────────────────────────
-	for _, u := range users {
-		log.Printf("Nome: %s | Email: %s | Telefone: %s | CreatedAt: %s", u.Nome, u.Email, u.Telefone, u.CreatedAt)
-	}
+	log.Println("processo finalizado com sucesso 🚀")
 }
